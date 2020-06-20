0de673d93c3ba3250212201e9de7e3c6a04ad572
Rukash <rafael.ieel@hotmail.com>
2020-06-20 13:18:49 -0300

    Module 02

---
+++ Module-02/Module-02/scripts.js
@@ -0,0 +1,26 @@
+const modalOverlay = document.querySelector(".modal_overlay");
+const modal = document.querySelector(".modal");
+const cards = document.querySelectorAll(".card");
+
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const contentId = card.getAttribute("id");
+    modalOverlay.classList.add("active");
+    modalOverlay.querySelector(
+      "iframe"
+    ).src = `https://rocketseat.com.br/${contentId}`;
+  });
+}
+
+document.querySelector("#close").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+  modalOverlay.querySelector("iframe").src = "";
+});
+
+document.querySelector("#max").addEventListener("click", () => {
+  modal.classList.add("maximize");
+});
+
+document.querySelector("#min").addEventListener("click", () => {
+  modal.classList.remove("maximize");
+});
