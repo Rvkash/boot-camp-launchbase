401ab8928fa2f64c828278a679830710f4b316bd
Rukash <rafael.ieel@hotmail.com>
2020-06-21 10:30:09 -0300

    fix: iframe page courses

--- Module-03/Module-03/public/scripts.js
+++ Module-03/Module-03/public/scripts.js
@@ -1,26 +1,8 @@
-const modalOverlay = document.querySelector('.modal_overlay')
-const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
 
 for (const card of cards) {
-  card.addEventListener('click', () => {
-    const contentId = card.getAttribute('id')
-    modalOverlay.classList.add('active')
-    modalOverlay.querySelector(
-      'iframe'
-    ).src = `https://rocketseat.com.br/${contentId}`
+  card.addEventListener('click', function () {
+    const idValue = card.getAttribute('id')
+    window.location.href = `/courses/${idValue}`
   })
 }
-
-document.querySelector('#close').addEventListener('click', () => {
-  modalOverlay.classList.remove('active')
-  modalOverlay.querySelector('iframe').src = ''
-})
-
-document.querySelector('#max').addEventListener('click', () => {
-  modal.classList.add('maximize')
-})
-
-document.querySelector('#min').addEventListener('click', () => {
-  modal.classList.remove('maximize')
-})
