37a0b20dd872aaec77a9ddfbe24b1f0e22351bca
Rukash <rafael.ieel@hotmail.com>
2020-06-24 20:57:35 -0300

    add: browser-sync and functions

--- Module-04/Module-04/public/scripts.js
+++
@@ -1,8 +0,0 @@
-const cards = document.querySelectorAll('.card')
-
-for (const card of cards) {
-  card.addEventListener('click', function () {
-    const idValue = card.getAttribute('id')
-    window.location.href = `/courses/${idValue}`
-  })
-}
