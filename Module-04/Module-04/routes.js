c24bdee5983c7527ae3f8bd2e3e36ecaa1463632
Rukash <rafael.ieel@hotmail.com>
2020-06-27 17:42:59 -0300

    add: route put and method override

--- Module-04/Module-04/routes.js
+++ Module-04/Module-04/routes.js
@@ -2,11 +2,17 @@ const express = require('express')
 const routes = express.Router()
 const teachers = require('./teachers')
 
+// http: verbs
+// get: receber RESOURCE -> algo real
+// post: Criar ou salva RESOURCE COM DADOS ENVIADOS
+// put: atualizar um RESOURCE
+// delete: apagar UM RESOURCE
+
 routes.get('/', function (req, res) {
   return res.redirect('/teachers')
 })
 
-routes.get('/teachers', function (req, res) {
+routes.get('/teachers', function (req, res) { // mesmo caminho usando verbos diferentes
   return res.render('teachers/index')
 })
 
@@ -16,7 +22,11 @@ routes.get('/teachers/create', function (req, res) {
 
 routes.get('/teachers/:id', teachers.show)
 
-routes.post('/teachers', teachers.post)
+routes.get('/teachers/:id/edit', teachers.edit)
+
+routes.post('/teachers', teachers.post) // mesmo caminho usando verbos diferentes
+
+routes.put('/teachers', teachers.put) // mesmo caminho usando verbos diferentes
 
 routes.get('/student', function (req, res) {
   return res.send('student')
