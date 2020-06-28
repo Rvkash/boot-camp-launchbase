0f76f4d0b1584613660e36ce8b548ca211b204c5
Rukash <rafael.ieel@hotmail.com>
2020-06-28 11:55:17 -0300

    add: routes students

--- Module-04/Module-04/routes.js
+++ Module-04/Module-04/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
-const teachers = require('./teachers')
+const teachers = require('./controllers/teachers')
+const students = require('./controllers/students')
 
 // http: verbs
 // get: receber RESOURCE -> algo real
@@ -13,23 +14,22 @@ routes.get('/', function (req, res) {
 })
 
 routes.get('/teachers', teachers.index)
-
-routes.get('/teachers/create', function (req, res) {
-  return res.render('teachers/create')
-})
-
+routes.get('/teachers/create', teachers.create)
 routes.get('/teachers/:id', teachers.show)
-
 routes.get('/teachers/:id/edit', teachers.edit)
-
 routes.post('/teachers', teachers.post) // mesmo caminho usando verbos diferentes
-
 routes.put('/teachers', teachers.put) // mesmo caminho usando verbos diferentes
-
 routes.delete('/teachers', teachers.delete)
 
-routes.get('/student', function (req, res) {
-  return res.send('student')
+routes.get('/students', students.index)
+routes.get('/students/create', (req, res) => {
+  return res.render('students/create')
 })
+routes.get('students/create', students.create)
+routes.get('/students/:id', students.show)
+routes.get('/students/:id/edit', students.edit)
+routes.post('/students', students.post)
+routes.put('/students', students.put)
+routes.delete('/students', students.delete)
 
 module.exports = routes
